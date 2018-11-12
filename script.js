f67872a7424c2941641ebb8761630b3956931995
JulieBio <juliebio69@gmail.com>
2018-11-12 19:59:50 +0100

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// par défaut dans React
-//import "isomorphic-fetch"
 
 const getTemp = function () {
     const cityName = document.getElementById("city").value;
@@ -11,17 +9,8 @@ const getTemp = function () {
         })
         .then(function (myJson) {
 
-            //permet d'avoir tout le json
-            //console.log(myJson);
-            //pour n'avoir que la température
-            //console.log(myJson.main.temp)
-            //on peut utiliser Math.ceil ou autre en fonction de l'arrondi qu'on souhaite
-            //document.getElementById("result").innerHTML = Math.round(myJson.main.temp) + " °C"
-            //pour ecrire la même chose en ES6:
-            //toutes les chaines de caractère entre backtick avec accolades
             document.getElementById("result").innerHTML = `${Math.round(myJson.main.temp)} °C`
         })
 
-    //document.getElementById("result").innerHTML = cityName
 
 }
